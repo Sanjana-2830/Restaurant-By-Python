ce56d4c6eb43de2c579c53b9b4c7a3a3fec91362
sanjana8920 <sanjanavats2830gmail.com>
2026-05-06 10:19:19 +0530

    User Order

--- hotelmenu.py
+++ hotelmenu.py
@@ -10,3 +10,11 @@ menu = {
 print("Welcome to Restaurant By Python")
 print("Pizza: Rs90\nBurger: Rs70\nCoffee: Rs110\nSalad: Rs50\nPasta: Rs80")
 
+order_total = 0
+item_1 = input("Enter the name of item you want to order = ")
+if item_1 in menu:
+    order_total +=menu[item_1]
+    print(f"Your {item_1} hass been added to your order")
+else:
+    print(f"Ordered item {item_1} is not available yet!")
+
